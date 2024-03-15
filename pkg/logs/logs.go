af054e22ce230ece81773c328a418d1a0a4d34e7
Nikolay Chupin <nickchupin@yandex.com>
2024-03-16 02:32:48 +0700

    [develop] logs

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -1,1 +1,26 @@
 package leshy_logs
+
+import (
+	"log/slog"
+
+	leshy_component "github.com/njxxdev/leshy/pkg/component"
+)
+
+type Logger struct {
+	name string
+	log  *slog.Logger
+}
+
+func (comp *Logger) GetInstance() leshy_component.Component {
+	return comp
+}
+
+func (comp *Logger) GetName() string { return comp.name }
+
+func NewAPIServer(name string) *Logger {
+
+	return &Logger{
+		name: name,
+		log:  slog.New(slog.Default().Handler()),
+	}
+}
