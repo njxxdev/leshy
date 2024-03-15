d01a6b72e4a23128724b54106ce1d631be39ee99
Nikolay Chupin <nickchupin@yandex.com>
2024-03-16 03:26:48 +0700

    [develop] http server new version

---
+++ pkg/api/http2/api.go
@@ -0,0 +1,60 @@
+package leshy_http2
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	path    string
+	methods map[string]func(*gin.Context)
+}
+
+type APIServer struct {
+	name   string
+	engine *gin.Engine
+
+	handlers []handler
+}
+
+func (server *APIServer) GetInstance() interface{} {
+	return server
+}
+
+func (server *APIServer) GetName() string { return server.name }
+
+func (server *APIServer) updateEngine(handlers []handler) {
+	for _, h := range handlers {
+		for method, process := range h.methods {
+			server.engine.Handle(method, h.path, process)
+		}
+	}
+}
+
+func New(name string, engine *gin.Engine) *APIServer {
+	engineE := engine
+	if engineE == nil {
+		engineE = gin.Default()
+	}
+	return &APIServer{
+		name:   name,
+		engine: engineE,
+	}
+}
+
+func (server *APIServer) Append(
+	paths []string,
+	methods map[string]func(*gin.Context),
+) *APIServer {
+	handlers := []handler{}
+	for _, path := range paths {
+		handlers = append(handlers,
+			handler{
+				path:    path,
+				methods: methods,
+			},
+		)
+	}
+	server.handlers = append(server.handlers, handlers...)
+	server.updateEngine(handlers)
+	return server
+}
